128b645f51b1460a0a3e5275d0fc9d26ab495a30
rhianne davidson <rhianne.davidson@sky.uk>
2023-02-11 14:51:14 +0000

    unit tests added for converting converttodto and convertoentity methods

--- Application/src/test/java/com/example/application/controller/CarControllerTest.java
+++ Application/src/test/java/com/example/application/controller/CarControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.application.controller;
 
 import com.example.application.entities.Car;
+import com.example.application.entities.CarDTO;
 import com.example.application.repository.CarRepository;
 import com.example.application.service.CarService;
 import org.junit.jupiter.api.Assertions;
@@ -10,10 +11,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class CarControllerTest {
@@ -28,7 +32,7 @@ public class CarControllerTest {
 
 
 //    @Test
-//    public void postMethodReturnsCreated() {
+//    public void carMethodReturnsCreated() {
 ////        Car mockCar = new Car("VW", "Tiguan", 2005, 3500, 56000, "red");
 //
 //        ResponseEntity<Map<String, String>> response = carController,
@@ -45,4 +49,37 @@ public class CarControllerTest {
         );
     }
 
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @Test
+    public void whenConvertCarEntityToCarDTO_thenCorrect() {
+        Car car = new Car("VW", "Tiguan", 2005, 3500, 56000, "red");
+
+        CarDTO carDto = modelMapper.map(car, CarDTO.class);
+        assertEquals(car.getBrand(), carDto.getBrand());
+        assertEquals(car.getModel(), carDto.getModel());
+        assertEquals(car.getYear(), carDto.getYear());
+        assertEquals(car.getPrice(), carDto.getPrice());
+        assertEquals(car.getMileage(), carDto.getMileage());
+        assertEquals(car.getColour(), carDto.getColour());
+    }
+
+    @Test
+    public void whenConvertCarDTOToCarEntity_thenCorrect() {
+        CarDTO carDto = new CarDTO();
+        carDto.setBrand("1");
+        carDto.setModel("Tiguan");
+        carDto.setYear(2022);
+        carDto.setPrice(15000);
+        carDto.setMileage(10);
+        carDto.setColour("Midnight Blue");
+
+        Car car = modelMapper.map(carDto, Car.class);
+        assertEquals(carDto.getBrand(), car.getBrand());
+        assertEquals(carDto.getModel(), car.getModel());
+        assertEquals(carDto.getYear(), car.getYear());
+        assertEquals(carDto.getPrice(), car.getPrice());
+        assertEquals(carDto.getMileage(), car.getMileage());
+        assertEquals(carDto.getColour(), car.getColour());
+    }
 }
