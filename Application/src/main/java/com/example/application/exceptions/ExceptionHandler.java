130eca1f90cd7f808414a63d04d5b56d93cdf783
TrevinKM <t.mabarana@lancaster.ac.uk>
2023-02-13 16:18:40 +0000

    Added exception class

---
+++ Application/src/main/java/com/example/application/exceptions/ExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.example.application.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class ExceptionHandler extends RuntimeException {
+}
